8afdb7e28976811f34c7d32520a09fa2fe2fdd4c
Doober <doober@Doobers-MacBook-Pro.local>
2023-06-24 20:44:41 -0400

    src updates

--- src/trading/oms.rs
+++ src/trading/oms.rs
@@ -1,14 +1,11 @@
-
+use std::cell::RefCell;
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Clone)]
-struct Order {
-	id: String,
-	price: f64,
-	qty: f64,
-	position_idx: u8,
-	created_time: i32,
-	updated_time: i32,
+type OrderMap = RefCell<HashMap<String, Order>>;
+
+enum AltReturn<'a> {
+	SomeOrder((&'a OrderMap, Order)),
+	SomeOrderId((&'a OrderMap, String))
 }
 
 #[derive(Clone)]
@@ -22,114 +19,148 @@ enum OrderPosition {
 #[derive(Clone)]
 enum OrderStatus {
 	Pending(OrderPosition),
-	Active(OrderPosition),
-	Cancelled(OrderPosition)
+	Active(OrderPosition)
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct Order {
+	id: String,
+	price: f64,
+	qty: f64,
+	position_idx: u8,
+	created_time: i32,
+	updated_time: i32,
 }
 
 #[derive(Debug, PartialEq)]
 struct Oms {
-	sell_side_orders_active: HashMap<String, Order>,
-	sell_side_orders_pending: HashMap<String, Order>,
-	buy_side_orders_active: HashMap<String, Order>,
-	buy_side_orders_pending: HashMap<String, Order>
+	sell_side_orders_active: OrderMap,
+	sell_side_orders_pending: OrderMap,
+	buy_side_orders_active: OrderMap,
+	buy_side_orders_pending: OrderMap
 }
 
 impl Oms {
 
 	pub fn new() -> Oms {
 		Oms { 
-			sell_side_orders_active: HashMap::new(),
-			sell_side_orders_pending: HashMap::new(),
-			buy_side_orders_active: HashMap::new(),
-			buy_side_orders_pending: HashMap::new()
+			sell_side_orders_active: RefCell::new(HashMap::new()),
+			sell_side_orders_pending: RefCell::new(HashMap::new()),
+			buy_side_orders_active: RefCell::new(HashMap::new()),
+			buy_side_orders_pending: RefCell::new(HashMap::new())
 		}
 	}
+	
+	pub fn handle_mapping(&mut self, order_value: OrderStatus) -> AltReturn {
 
-	pub fn add_order (&mut self, order: OrderStatus) {
-		match order {
+		match order_value {
+
 			OrderStatus::Active(order_position) => {
+
 				match order_position {
 					OrderPosition::BuySide(order) => {
-						self.buy_side_orders_active
-							.insert(order.id.clone(), order);
+						AltReturn::SomeOrder((&self.buy_side_orders_active, order))
 					}
 
 					OrderPosition::SellSide(order) => {
-						self.sell_side_orders_active
-							.insert(order.id.clone(), order);
+						AltReturn::SomeOrder((&self.sell_side_orders_active, order))
 					}
 
-					_ => todo!()
+					OrderPosition::BuySideId(order_id) => {
+						AltReturn::SomeOrderId((&self.buy_side_orders_active, order_id))
+					}
+
+					OrderPosition::SellSideId(order_id) => {
+						AltReturn::SomeOrderId((&self.sell_side_orders_active, order_id))
+					}
 				}
 			}
 
 			OrderStatus::Pending(order_position) => {
+
 				match order_position {
 					OrderPosition::BuySide(order) => {
-						self.buy_side_orders_pending
-							.insert(order.id.clone(), order);
+						AltReturn::SomeOrder((&self.buy_side_orders_pending, order))
 					}
 
 					OrderPosition::SellSide(order) => {
-						self.sell_side_orders_pending
-							.insert(order.id.clone(), order);
+						AltReturn::SomeOrder((&self.sell_side_orders_pending, order))
 					}
 
-					_ => todo!()
+					OrderPosition::BuySideId(order_id) => {
+						AltReturn::SomeOrderId((&self.buy_side_orders_pending, order_id))
+					}
+
+					OrderPosition::SellSideId(order_id) => {
+						AltReturn::SomeOrderId((&self.sell_side_orders_pending, order_id))
+					}
 				}
 			}
-
-			OrderStatus::Cancelled(_) => todo!()
 		}
 	}
+	
+	pub fn add_order (&mut self, order_value: OrderStatus) {
 
-	pub fn get_order(&self, order: OrderStatus) -> &Order {
-		match order {
-			OrderStatus::Active(order_position) => {
-				match order_position {
-					OrderPosition::BuySideId(order_id) => {
-						let value = self.buy_side_orders_active
-							.get(&order_id)
-							.expect("Failed to get active buy side order");
+		let AltReturn::SomeOrder((map, order)) = self.handle_mapping(order_value) else { panic!() };
 
-						value
-					}
+		map
+			.borrow_mut()
+			.insert(order.id.clone(), order);
+	}
 
-					OrderPosition::SellSideId(order_id) => {
-						let value = self.sell_side_orders_active
-							.get(&order_id)
-							.expect("Failed to get active sell side order");
+	pub fn delete_order(&mut self, order_value: OrderStatus) {
 
-						value
-					}
+		let AltReturn::SomeOrderId((map, order_id)) = self.handle_mapping(order_value) else { panic!() };
 
-					_ => todo!()
-				}
-			}
+		map 
+			.borrow_mut()
+			.remove(&order_id);
+	}
 
-			OrderStatus::Pending(order_position) => {
-				match order_position {
-					OrderPosition::BuySideId(order_id) => {
-						let value = self.buy_side_orders_pending
-							.get(&order_id)
-							.expect("Failed to get pending buy side order");
+	pub fn get_order(&mut self, order_value: OrderStatus) -> Order {
 
-						value
-					}
+		let AltReturn::SomeOrderId((map, order_id)) = self.handle_mapping(order_value) else { panic!() };
 
-					OrderPosition::SellSideId(order_id) => {
-						let value = self.sell_side_orders_pending
-							.get(&order_id)
-							.expect("Failed to get pending sell side order");
+		let binding_map = map.borrow();
 
-						value
-					}
+		let anw = binding_map
+			.get(&order_id)
+			.expect("Failed to get order");
 
-					_ => todo!()
-				}
-			}
+		anw.clone()
 
-			_ => todo!()
-		}
+	}
+	// Will return current inventory delta
+	pub fn get_inventory_delta(&self) -> f64 {
+
+		let bid_delta: f64 = self.buy_side_orders_active
+			.borrow()
+			.values()
+			.map(|order| order.qty)
+			.sum();
+
+		println!("{:?}", bid_delta);
+
+		let ask_delta: f64 = self.sell_side_orders_active
+			.borrow()
+			.values()
+			.map(|order| order.qty)
+			.sum();
+
+		println!("{:?}", ask_delta);
+
+		let value = bid_delta - ask_delta;
+
+		value
+	}
+
+	pub fn get_size_to_target(&self, target_delta: f64) -> f64 {
+		
+		let current_delta = self
+			.get_inventory_delta();
+
+		let anw = (current_delta - target_delta).abs();
+
+		anw
 	}
 }
--- src/trading/orderbook.rs
+++ src/trading/orderbook.rs
@@ -1,12 +1,13 @@
-use std::rc::Rc;
 use std::cmp::Ordering;
 use std::cell::RefCell;
 use std::collections::BTreeMap;
 use ordered_float::OrderedFloat;
+use std::time::{ SystemTime, UNIX_EPOCH };
 
-type BidsMap = Rc<RefCell<BTreeMap<OrderedFloat<f64>, RestingOrder>>>;
-type AsksMap = Rc<RefCell<BTreeMap<OrderedFloat<f64>, RestingOrder>>>;
+type BidsMap = RefCell<BTreeMap<OrderedFloat<f64>, RestingOrder>>;
+type AsksMap = RefCell<BTreeMap<OrderedFloat<f64>, RestingOrder>>;
 
+#[derive(Clone)]
 enum RestingOrderType {
     BidOrder(RestingOrder),
     AskOrder(RestingOrder),
@@ -18,17 +19,30 @@ enum RestingOrderType {
 struct RestingOrder {
     price: f64,
     size: f64,
-    ts: i32
+    ts: u128
 }
 
 #[derive(Clone, Debug, PartialEq)]
 struct Orderbook {
     asks: AsksMap,
     bids: BidsMap,
-    last_update_time: i32,
+    last_update_time: u128,
 }
 
 impl Orderbook {
+    fn new() -> Orderbook {
+        let current_time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Failed to get duration")
+            .as_millis();
+
+        Orderbook {
+            asks: RefCell::new(BTreeMap::new()),
+            bids: RefCell::new(BTreeMap::new()),
+            last_update_time: current_time
+        }
+    }
+
     // Inserts resting order into orderbook 
     fn insert_order (&mut self, order: RestingOrderType) {
 
@@ -54,13 +68,6 @@ impl Orderbook {
             _ => todo!()
         }
     }
-    // Returns all asks
-    fn get_asks(&self) -> BidsMap {
-        let binding = self.asks
-            .clone();
-
-        binding
-    }
     // Returns ask closest to mid-price
     fn get_ask(&self) -> RestingOrder {
 
@@ -72,13 +79,6 @@ impl Orderbook {
             .expect("Failed to get first key value");
 
         RestingOrder { price: value.price, size: value.size,  ts: value.ts }
-    }
-    // Returns all bids
-    fn get_bids(&self) -> AsksMap {
-        let binding = self.bids
-            .clone();
-
-        binding
     }
     // Returns bid closest to mid-price
     fn get_bid(&self) -> RestingOrder {
@@ -92,8 +92,71 @@ impl Orderbook {
 
         RestingOrder { price: value.price, size: value.size, ts: value.ts }
     }
+    // Returns all asks
+    fn get_asks(&self) -> &BidsMap {
+        let asks = &self.asks;
+
+        asks
+    }
+    // Returns all bids
+    fn get_bids(&self) -> &AsksMap {
+        let bids = &self.bids;
+
+        bids
+    }
+    // Calculates the current orderbook skew
+    fn get_ordebook_skew(&self) -> f64 {
+
+        let buy_side_depth: f64 = self
+            .bids
+            .borrow()
+            .values()
+            .map(|order| order.size)
+            .sum();
+
+        let sell_side_depth: f64 = self
+            .asks
+            .borrow()
+            .values()
+            .map(|order| order.size)
+            .sum();
+
+        let value = buy_side_depth.ln() - sell_side_depth.ln();
+
+        value
+    }
+    // Gets orderbook mid price
+    fn get_mid_price(&self) -> f64 {
+        let bid_price = self
+            .get_bid()
+            .price;
+
+        let ask_price = self
+            .get_ask()
+            .price;
+
+        let mid_price = (bid_price + ask_price) / 2.0;
+
+        mid_price
+    }
+    // Gets orderbook spread
+    fn get_orderbook_spread(&self) -> f64 {
+        let bid_price = self
+            .get_bid()
+            .price;
+
+        let ask_price = self
+            .get_ask()
+            .price;
+
+        let spread = ask_price - bid_price;
+
+        spread
+    }
     // Checks to see if your trade size can be filled in full at a specific price
     // ToDo: Determine what should happen at equal
+    // true = Trade is safe 
+    // false = Trade is unsafe
     fn safety_check_size (&self, price: RestingOrderType, size: f64) -> bool {
 
         match price {
@@ -113,7 +176,6 @@ impl Orderbook {
                     Ordering::Equal => true,
                     Ordering::Less => true
                 }
-
             }
 
             RestingOrderType::AskPrice(ask) => {
@@ -135,6 +197,18 @@ impl Orderbook {
             }
 
             _ => todo!()
+        }
+    }
+    // Safety Check
+    fn safety_check_spread(&self, max_spread: f64) -> bool {
+        // Goal of this function will check the spread to see if it is too volatile
+        // If so stop trading until things calm down
+        let current_spread = self.get_orderbook_spread();
+
+        if current_spread > max_spread {
+            false
+        } else {
+            true
         }
     }
 }
